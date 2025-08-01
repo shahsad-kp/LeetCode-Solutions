e0376b450e17871a8466b68e20000f6d29dd1891
Shahsad KP <shahsadkpklr@gmail.com>
2025-08-01 07:21:32 +0530

    #118

---
+++ questions/118.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+class Solution:
+    __question__ = 'Pascal\'s Triangle'
+    __leetcode__ = 'https://leetcode.com/problems/pascals-triangle'
+    __test_cases__ = [
+        {
+            'input': [
+                5
+            ],
+            'output': [
+                [1],
+                [1, 1],
+                [1, 2, 1],
+                [1, 3, 3, 1],
+                [1, 4, 6, 4, 1]
+            ]
+        },
+        {
+            'input': [
+                1
+            ],
+            'output': [
+                [1]
+            ]
+        }
+    ]
+
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+
+        for row in range(numRows):
+            new_row = [1] * (row + 1)
+            for i in range(1, row):
+                new_row[i] = triangle[row - 1][i - 1] + triangle[row - 1][i]
+            triangle.append(new_row)
+
+        return triangle
