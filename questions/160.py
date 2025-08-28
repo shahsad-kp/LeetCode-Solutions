ea853ee520bb107bd5b302c48c1635c6d987bec0
Shahsad KP <shahsadkpklr@gmail.com>
2025-08-28 19:52:16 +0530

    #160

---
+++ questions/160.py
@@ -0,0 +1,64 @@
+from typing import Optional, List
+
+from run_leetcode_solutions.base_solution import BaseSolution
+from run_leetcode_solutions.test_cases import TestCases
+from run_leetcode_solutions.test_value import TestValue
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def build_intersecting_lists(a_list, b_list, skip_a, skip_b):
+    # Build list A
+    headA = ListNode(a_list[0])
+    currA = headA
+    nodesA = [headA]
+    for val in a_list[1:]:
+        node = ListNode(val)
+        currA.next = node
+        currA = node
+        nodesA.append(node)
+
+    # Build list B
+    headB = ListNode(b_list[0])
+    currB = headB
+    nodesB = [headB]
+    for val in b_list[1:]:
+        node = ListNode(val)
+        currB.next = node
+        currB = node
+        nodesB.append(node)
+
+    # Connect intersection
+    if 0 <= skip_a < len(nodesA) and 0 <= skip_b < len(nodesB):
+        nodesB[skip_b].next = nodesA[skip_a]
+
+    return headA, headB
+
+class Solution(BaseSolution):
+    __solution_method__ = 'getIntersectionNode'
+    automatic_tests = TestCases(
+        TestValue(
+            inputs=[
+                *build_intersecting_lists(a_list=[4,1,8,4,5], b_list=[5,6,1,8,4,5], skip_a=2, skip_b=3),
+            ],
+            expected=8
+        )
+    )
+
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        a_list = []
+        head_a = headA
+        while head_a:
+            a_list.append(head_a)
+            head_a = head_a.next
+        head_b = headB
+        while head_b:
+            if head_b in a_list:
+                return head_b.val
+            head_b = head_b.next
+
+        return None
